665e7c72b280a0d32222be7dafee249a5966560f
felipesilva543 <felipesilva543@alu.ufc.br>
2017-08-28 17:25:41 -0300

    Primeiras alterações

--- JunkFood/main.cpp
+++ JunkFood/main.cpp
@@ -1,25 +1,107 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-class JunkFood{
+class Cliente{
+  private:
     float saldo;
-    JunkFood(float _saldo = 0){
+  public:
+    Cliente(float _saldo = 0){
         saldo = _saldo;
     }
 
-    float setSaldo( float valor){
-        saldo += valor;
+    bool inserirDin(float _valor){
+        if (_valor > 0){
+            this->saldo += _valor;
+            return true;
+        }
+        return false;
     }
 
-    void getSaldo(){
-        return this->getSaldo();
+    float getSaldo(){
+        return this->saldo;
+    }
+
+    float getTroco(){
+        float x = this->saldo;
+        this->saldo = 0;
+        return x;
     }
 };
 
+class Produto{
+  private:
+   // int id;
+    string nome;
+    int qnt;
+    float valor;
+  public:
+    Produto(string _nome = "", int _qtd = 0, float _valor = 0){
+        this->id = _id;
+        this->nome = _nome;
+        this->qnt = _qtd;
+        this->valor = _valor;
+    }
 
-int main(int argc, char *argv[])
+};
+
+class JunkFood{
+  private:
+    int espirais;
+    vector<Produto> produtos;
+  public:
+    JunkFood(int _espirais = 0){
+        this->espirais = _espirais;
+        produtos.resize(espirais);
+    }
+
+    void alterarProd(){
+
+    }
+
+};
+
+
+
+int main()
 {
-    cout << "Hello World!" << endl;
+    Cliente cliente;
+
+    cout << "Ola Cliente!" << endl
+         << "Comandos:" << endl
+         << "inserirDin _valor" << endl
+         << "saldo" << endl
+         << "troco" << endl
+         << "fim" << endl << endl
+
+         << "Ola Admin!" << endl
+         << "Comandos:" << endl
+         << "iniciar _qtd" << endl
+
+         << "fim" << endl;
+
+    string op;
+    cout << endl;
+
+    while (op != "fim") {
+        cin >> op;
+        if(op == "inserirDin"){
+            float x;
+            cin >> x;
+            if(cliente.inserirDin(x)){
+                cout << "Suceso!"<< endl;
+            }else{
+                cout << "Erro!" << endl;
+            }
+        }
+        if (op == "saldo"){
+           cout << cliente.getSaldo() << " Reais." << endl;
+        }
+        if (op == "troco"){
+            cout << "Você retirou " << cliente.getTroco() << " Reais." << endl;
+        }
+    }
+
+
     return 0;
 }
