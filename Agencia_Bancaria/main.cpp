fbfe32c9cef16c08281fb0c35bf6585b21bfb866
felipesilva543 <felipesilva543@alu.ufc.br>
2017-09-12 16:11:38 -0300

    Agencia Bancaria

--- Agencia_Bancaria/main.cpp
+++ Agencia_Bancaria/main.cpp
@@ -74,12 +74,12 @@ public:
         return ss.str();
     }
 
-    bool getAtiva(){
-        return this->ativa;
-    }
-
     void setAtiva(){
         this->ativa = !this->ativa;
+    }
+
+    bool getAtiva(){
+        return this->ativa;
     }
 
 //    bool tranferencia(Conta _outra, float _valor){
@@ -97,22 +97,6 @@ class Cliente{
         this->cpf = _cpf;
     }
 
-    bool addConta(string _numero){
-
-    }
-
-//    int endConta(int _numero){
-//        for(Conta elemento : contas){
-//            if(elemento.getNumero() == _numero){
-//                if(elemento.getSaldo() > 0){
-//                    return -2;
-//                }
-//                elemento.setAtiva();
-//                return true;
-//            }
-//        }
-//    }
-
     string getCpf(){
         return this->cpf;
     }
@@ -124,6 +108,22 @@ class Cliente{
     void setConta(int _num){
         contas.push_back(Conta(_num));
     }
+
+    int endConta(int _numero){
+        for(Conta elemento : contas){
+            if(elemento.getNumero() == _numero){
+                if(elemento.getSaldo() > 0){
+                    return -3; // Só pode encerrar com saldo zerado
+                }
+                if(elemento.getAtiva() == false){
+                    return -2;
+                }
+                elemento.setAtiva();
+                return 1;
+            }
+        }
+        return -1;
+    }
 };
 
 class Agencia{
@@ -131,28 +131,23 @@ class Agencia{
   public:
     Agencia(){}
 
-    bool addCliente(string _cpf){
-        for(Cliente elemento : clientes){
-            if(elemento.getCpf() == _cpf){
-                return false;
-            }
-        }
-        clientes.push_back(Cliente(_cpf));
-        return true;
-    }
-
     int abrirConta(string _cpf){
-        for(Cliente elemento : clientes){
-            if(elemento.getCpf() != _cpf){
-                return -1;
-            }
-        }
-        for(Cliente elemento : clientes){
-           // int cont;
-            if(elemento.getConta().size() > 2){
-                return -2;
-            }
-        }
+
+        //Pra saber se tem duas contas ativas
+//        for(Cliente elemento : clientes){
+//           // int cont;
+//            if(elemento.getConta().size() == 2){
+//                int x;
+//                for(Conta elem : clientes){
+//                    if(elem.getAtiva() == true){
+//                        x += 1;
+//                    }
+//                }
+//                if(x == 2){
+//                    return -2;
+//                }
+//            }
+//        }
         for(Cliente& elemento : clientes){
             if(elemento.getCpf() == _cpf){
                 elemento.setConta(controle);
@@ -162,11 +157,37 @@ class Agencia{
         }
     }
 
-    void showCliente(){
+    bool addCliente(string _cpf){
         for(Cliente elemento : clientes){
-            cout << elemento.getCpf() << endl;
-
+            if(elemento.getCpf() == _cpf){
+                return false;
+            }
         }
+        clientes.push_back(Cliente(_cpf));
+        abrirConta(_cpf);
+        return true;
+    }
+
+    string showAllCliente(){
+        stringstream ss;
+        int x = 1;
+        vector<Conta> contas;
+        vector<int> contasAti;
+        for(Cliente elemento : clientes){
+            contas = elemento.getConta();
+            for(Conta elemento2 : contas){
+                if(elemento2.getAtiva()){
+                    contasAti.push_back(elemento2.getNumero());
+                }
+            }
+            ss << x << " - " << elemento.getCpf() << "[ ";
+            for(int i = 0; i < (int)contasAti.size(); i++){
+                ss << contasAti[i] << " ";
+            }
+            ss << "]" << endl;
+            x += 1;
+        }
+        return ss.str();
     }
 };
 
