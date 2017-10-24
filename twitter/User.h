f4e647ac02a3b81e5fb20fc9261e80a232a40f91
iago-lima <iagoliveiralima3@gmail.com>
2017-10-24 16:44:36 -0300

    Tentando resolver bugs - FAIL

--- twitter/User.h
+++ twitter/User.h
@@ -7,14 +7,14 @@
 
 using namespace std;
 
-
+//class Tweett;
 
 class User{
     string username;
     Repository<User> seguidores;
     Repository<User> seguidos;
     Repository<Tweett> myTweets;
-    vector<Tweett> timeLine;
+    Repository<Tweett> timeLine;
     int unreadCount;
     int nextTwId;
 
@@ -34,10 +34,10 @@ public:
 //    }
 
     vector<Tweett> getUnread(){
-        vector<Tweett> aux;
-
-        for(int i = ((int)timeLine.size() - unreadCount); i < ((int)timeLine.size()); i++){
-            aux.push_back(timeLine[i]);
+        vector<Tweett> aux = timeLine.getValues();
+        vector<Tweett> aux2;
+        for(int i = ((int)aux.size() - unreadCount); i < ((int)aux.size()); i++){
+            aux2.push_back(aux[i]);
         }
         return aux;
     }
@@ -46,38 +46,46 @@ public:
         return this->username;
     }
 
-    Repository<User> getSeguidores(){
-        return this->seguidores;
+    vector<User> getSeguidores(){
+        return this->seguidores.getValues();
     }
 
-    Repository<User> getSeguidos(){
-        return this->seguidos;
+    vector<User> getSeguidos(){
+        return this->seguidos.getValues();
     }
 
-    Repository<Tweett> getMyTweets(){
-        return this->myTweets;
+    vector<Tweett> getMyTweets(){
+        return this->myTweets.getValues();
     }
 
-    vector<Tweett> getTimeLine(){
+    Repository<Tweett> getTimeLine(){
         return this->timeLine;
     }
 
     string toStringUser(){
         stringstream ss;
         ss << "Nome: " << getUserName() << endl;
+
         ss << "Seguidores: { ";
         vector<User> aux = seguidores.getValues();
         for(User elemento : aux){
-            ss << elemento.getUserName() << ", ";
+            ss << elemento.getUserName() << " ";
         }
-        string username;
-        Repository<User> seguidores;
-        Repository<User> seguidos;
-        Repository<Tweett> myTweets;
-        vector<Tweett> timeLine;
-        int unreadCount;
-        int nextTwId;
+        ss << "}" << endl;
 
+        ss << "Seguidos: { ";
+        vector<User> aux2 = seguidos.getValues();
+        for(User elemento: aux2){
+            ss << elemento.getUserName() << " ";
+        }
+        ss << "}" << endl;
+
+        ss << "Meus tweetts { ";
+        vector<Tweett> aux3 = myTweets.getValues();
+        for(Tweett elemento: aux3){
+            ss << "ID: " << elemento.getTwId() << ": " << elemento.getMsg() << endl;
+        }
+        ss << "}";
     }
 };
 
