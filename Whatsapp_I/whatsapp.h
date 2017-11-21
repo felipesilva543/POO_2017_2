a2cf11735c23f0e6d37f0d9cd615be6fc3af5661
felipesilva543 <felipesilva543@alu.ufc.br>
2017-11-21 20:57:00 -0300

    Semi conclusão do Whats

--- Whatsapp_I/whatsapp.h
+++ Whatsapp_I/whatsapp.h
@@ -3,7 +3,6 @@
 
 #include <sstream>
 #include <iostream>
-#include "whatsapp.h"
 #include "poo_aux.h"
 
 using namespace std;
@@ -23,8 +22,8 @@ public:
 
     string toStringZap(){
         stringstream ss;
-        ss << "[ Zap: " << userId;
-        ss << ": " << msg << " ]" << endl;
+        ss << "[" << userId;
+        ss << ": " << msg << "]" << endl;
 
         return ss.str();
     }
@@ -37,7 +36,7 @@ class User{
     map<string, Chat*> lista_chat;
     string userId;
 public:
-    User(string _userId){
+    User(string _userId = ""){
         userId = _userId;
     }
 
@@ -45,63 +44,22 @@ public:
         return poo::map_keys(lista_chat);
     }
 
-    string getOverview(){
-        stringstream ss;
-        for(auto par : lista_chat){
-            ss << "Chat: " << par.first << endl;
-            ss << "Qtd. msg não lidas: " << par.second->getUnreadCount(userId) << endl;
-        }
-        return ss.str();
-    }
+    string getOverview();
 
     void assertChat(string _chatId){
         if(lista_chat.count(_chatId) == 0)
-            throw _chatId + " não existe!";
+            throw "Usuário não faz parte do grupo " + _chatId;
     }
 
-    void invite(string _chatId, User * _user){
-        assertChat(_chatId);
-        Chat aux = *lista_chat[_chatId];
+    void invite(string _chatId, User * _user);
 
-        //se o usuario ja esta no chat, retorne
+    void leave(string _chatId);
 
-        //Abra um registro para esse usuario no chat
-        aux.lista_reg[_user->userId] = Registro(_user);
-        //Adicione esse chat na lista dos usuarios
-        _user->lista_chat[_chatId] = &aux;
-//            # se as mensagens de InOut estiverem habilitadas
-//                # envie uma mensagem avisando que o novo usuario chegou
-
-
-
-    }
-    void leave(string _chatId){
-        //Teste se voce tem esse chat - OK
-        assertChat(_chatId);
-        Chat aux = *lista_chat[_chatId];
-        //Se retire da lista do chat
-        aux.lista_reg.erase(this->userId);
-        //Retire o chat da sua lista
-        lista_chat.erase(aux.chatId);
-
-
-        //Se habilitado, mande mensagem avisando que voce saiu
-    }
-
-
-    void sendZap(string _msg, string _chatId){
-        //Teste se voce tem esse chat
-        assertChat(_chatId);
-        //Obtenha o objeto chat
-        Chat aux = *lista_chat[_chatId];
-        //Delegue para o chat entregar o objeto Zap que voce criou
-
-    }
+    void sendZap(string _msg, string _chatId);
 
     string toStringUser(){
         return this->userId;
     }
-
 
     friend class Chat;
 };
@@ -110,7 +68,7 @@ class Registro{
     User * user;
     int underCount{0};
 public:
-    Registro(User * _user){
+    Registro(User * _user = nullptr){
         user = _user;
     }
     void underCountPlus(int qtd){
@@ -129,13 +87,16 @@ class Chat{
     vector<Zap> lista_zap;
     map<string, Registro> lista_reg;
     static bool enableInOutMsgs;
-    static const string systemUsername = "system";
+    //static string systemUsername = "system";
 public:
-    Chat(string _chatId){
+    Chat(string _chatId = ""){
         chatId = _chatId;
     }
+    void setEnableInOutMsgs(bool valor){
+        this->enableInOutMsgs = valor;
+    }
 
-    vector<string> getUser(){
+    vector<string> getUsers(){
         vector<string> users;
         for(auto par : lista_reg)
             users.push_back(par.first);
@@ -150,7 +111,7 @@ public:
         lista_reg[_chatCreator->userId] = Registro(_chatCreator);
         //Se adicione a lista de grupos do usuario
         _chatCreator->lista_chat[this->chatId] = this;
-
+        return true;
     }
 
     void assertUser(string _user){
@@ -158,18 +119,8 @@ public:
             throw _user + " não existe!";
     }
 
-
     void deliverZap(Zap _zap){
-
-        /**
-         *  # se nao for um zap do sistema, teste se o usuario esta no chat
-        # adicione o zap na lista do grupo
-        # para cada usuario do grupo
-            # se ele nao for o emissor
-                # incremente o contador de nao lidos
-         */
-
-        if(_zap.getUserId() == "system"){
+        if(_zap.getUserId() == "System"){
             lista_zap.push_back(_zap);
             for(auto user : lista_reg){
                 user.second.underCountPlus(1);
@@ -177,7 +128,7 @@ public:
         }else{
             assertUser(_zap.getUserId());
             lista_zap.push_back(_zap);
-            for(auto user : lista_reg){
+            for(auto & user : lista_reg){
                 if(user.first != _zap.getUserId()){
                     user.second.underCountPlus(1);
                 }
@@ -195,44 +146,38 @@ public:
         return aux.getUnderCount();
     }
 
-    /**
-     *def getUnread(userId):
-    # teste se usuario existe no grupo
-    # crie lista de retorno
-    # veja quantas sao as mensagens nao lidas desse usuario
-    # já coletando mensagens do grupo que nao forem emitidas
-    # por esse usuario ate atingir a quantidade de nao lidas.
-    # zere a quantidade de mensagens nao lidas
-    # se nao houver mensagens, retorne que nao ha mensagens
-     */
-
     vector<Zap> getUnread(string _userId){
         assertUser(_userId);
         vector<Zap> retorno;
         int tmhVetZap = (int) lista_zap.size();
         int qtdNLidas = getUnreadCount(_userId);
+        Registro &auxReg = lista_reg[_userId];
 
-        if(tmhVetZap == 0){
-            retorno.push_back(Zap("System", "Não ha zaps!\n"));
+        if(!auxReg.getUnderCount()){
+            retorno.push_back(Zap("System", "Não ha zaps!"));
             return retorno;
         }
 
-        while(qtdNLidas != 0){
-            Zap zapAux = lista_zap[tmhVetZap - qtdNLidas];
+        while(qtdNLidas > 0){
+            Zap zapAux = lista_zap[tmhVetZap - 1];
             if(zapAux.getUserId() != _userId){
                 retorno.push_back(zapAux);
                 qtdNLidas -= 1;
+                tmhVetZap -= 1;
+            }else{
+                retorno.push_back(zapAux);
+                tmhVetZap -= 1;
             }
         }
 
-        Registro auxReg = lista_reg[_userId];
         auxReg.setUnderCount(0);
 
         return retorno;
     }
 
     bool hasUser(string _userId){
-        if(lista_reg.count(_userId))
+        int x = lista_reg.count(_userId);
+        if(x)
             return true;
         return false;
     }
@@ -244,7 +189,55 @@ public:
     friend class User;
 };
 
+string User::getOverview(){
+    stringstream ss;
+    for(auto par : lista_chat){
+        ss << "Chat: " << par.first;
+        ss << "{" << par.second->getUnreadCount(userId) << "}" << endl;
+    }
+    return ss.str();
+}
+
+void User::invite(string _chatId, User *_user){
+    assertChat(_chatId);
+    Chat * aux = lista_chat[_chatId];
+    bool t = aux->hasUser(_user->userId);
+    //se o usuario ja esta no chat, retorne
+    if(t)
+        return;
+    //Abra um registro para esse usuario no chat
+    aux->lista_reg[_user->userId] = Registro(_user);
+    //Adicione esse chat na lista dos usuarios
+    _user->lista_chat[_chatId] = aux;
+
+    //            # se as mensagens de InOut estiverem habilitadas
+    //                # envie uma mensagem avisando que o novo usuario chegou
+}
+
+void User::leave(string _chatId){
+    //Teste se voce tem esse chat - OK
+    assertChat(_chatId);
+    Chat * aux = lista_chat[_chatId];
+    //Se retire da lista do chat
+    aux->lista_reg.erase(this->userId);
+    //Retire o chat da sua lista
+    lista_chat.erase(aux->chatId);
+
+
+    //Se habilitado, mande mensagem avisando que voce saiu
+}
+
+void User::sendZap(string _msg, string _chatId){
+    //Teste se voce tem esse chat
+    assertChat(_chatId);
+    //Obtenha o objeto chat
+    Chat * aux = lista_chat[_chatId];
+    //Delegue para o chat entregar o objeto Zap que voce criou
+    Zap msg = Zap(this->userId, _msg);
+    aux->deliverZap(msg);
+}
+
+
 
 
 #endif // WHATSAPP
-
